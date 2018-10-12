81f884fa9f462ba652d7878658da224902201eef
RSysNoida <43948173+RSysNoida@users.noreply.github.com>
2018-10-12 15:59:07 +0530

    Update YearListItem.js

--- src/listitems/tracker-list/YearListItem.js
+++ src/listitems/tracker-list/YearListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList,StyleSheet } from "react-native";
 
 
 import TrackerListItem from "./TrackerListItem";
