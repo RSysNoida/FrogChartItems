35ee3877f76e553c9e7a5c658c8b0ded1d8f4369
RSysNoida <43948173+RSysNoida@users.noreply.github.com>
2018-10-12 15:58:43 +0530

    Update TrackerListItem.js

--- src/listitems/tracker-list/TrackerListItem.js
+++ src/listitems/tracker-list/TrackerListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image,StyleSheet } from "react-native";
 
 class TrackerListItem extends React.Component {
 
