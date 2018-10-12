9ba8d3627e2c3829858595ec16a41cbea6d11e15
RSysNoida <43948173+RSysNoida@users.noreply.github.com>
2018-10-12 15:58:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList,StyleSheet } from "react-native";
 
 import YearListItem from "./src/listitems/tracker-list/YearListItem";
 
